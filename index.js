8134c28eb5fded1b5670dfc329d8caa8f176a83a
onnyyonn <onnyyonn@users.noreply.github.com>
2021-05-10 01:45:36 +0000

    deploy: a26a13e34a537934db452b4d7ae21ea1ab97e9b6

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-$('p').linkify({
-    target: "_blank"
+document.addEventListener("click", (event) => {
+  const actionButton = event.target.closest(`[data-action]`);
+
+  if (actionButton) {
+    actionButton.closest("article").classList.toggle("expanded");
+  }
 });
+
+const timestamp = document.getElementById("build-timestamp");
+timestamp.innerText = new Date(timestamp.getAttribute("datetime")).toLocaleString();
---
+++ my_linkify.js
@@ -0,0 +1,3 @@
+$('p').linkify({
+    target: "_blank"
+});
