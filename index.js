85b31eaaefa395f35e7b1cf1394ebb1bfcf028e8
onnyyonn <onnyyonn@users.noreply.github.com>
2021-05-15 22:19:12 +0000

    deploy: 98c91b4353ce3426723bb4b9752ece0e353544c6

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-document.addEventListener("click", (event) => {
-  const actionButton = event.target.closest(`[data-action]`);
-
-  if (actionButton) {
-    actionButton.closest("article").classList.toggle("expanded");
+/* Exapand/Collapse with TAB key */
+var expanded = false;
+document.onkeydown=function(e){
+  if(e.keyCode == 9) {
+    expanded = !expanded;
+    document.querySelectorAll("details").forEach(detail => detail.open = expanded);
+    return false;
   }
-});
+}
 
-const timestamp = document.getElementById("build-timestamp");
-timestamp.innerText = new Date(timestamp.getAttribute("datetime")).toLocaleString();
+/* Convert url in plaintext to clickable link */
+<script src="https://cdn.jsdelivr.net/npm/linkifyjs@3.0.0-beta.3/dist/linkify.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/linkifyjs@3.0.0-beta.3/dist/linkify-element.min.js"></script>
+<script>document.querySelectorAll("article .details-content").forEach($ => linkifyElement($))</script>
