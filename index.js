b03648e3ec0a88b5d859b1fc9bc28862bb3dfe4c
onnyyonn <onnyyonn@users.noreply.github.com>
2021-05-15 22:34:22 +0000

    deploy: f6a7e9a0f2262478eb32e3567e85a9c390266846

--- index.js
+++ index.js
@@ -7,8 +7,3 @@ document.onkeydown=function(e){
     return false;
   }
 }
-
-/* Convert url in plaintext to clickable link */
-<script src="https://cdn.jsdelivr.net/npm/linkifyjs@3.0.0-beta.3/dist/linkify.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/linkifyjs@3.0.0-beta.3/dist/linkify-element.min.js"></script>
-<script>document.querySelectorAll("article .details-content").forEach($ => linkifyElement($))</script>
